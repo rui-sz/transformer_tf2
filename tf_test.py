e3a7260ba5a2aecdf4ed4867960e6cff904bd6bb
rui-sz <69101330+rui-sz@users.noreply.github.com>
2023-06-04 21:00:04 +0800

    基于tf 2.x 环境的transformer模型可以跑起来了。 下一步：代码优化，逻辑重构

--- tf_test.py
+++ tf_test.py
@@ -519,8 +519,8 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     self.warmup_steps = warmup_steps
     
   def __call__(self, step):
-    arg1 = tf.math.rsqrt(step)
-    arg2 = step * (self.warmup_steps ** -1.5)
+    arg1 = tf.math.rsqrt(tf.cast(step, tf.float32))
+    arg2 = tf.cast(step,tf.float32) * (self.warmup_steps ** -1.5)
     
     return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
